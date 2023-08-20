b4c41327e9fb7d36e2c8d9e20b1d5a1c2bf64898
jordan <jordanf828@gmail.com>
2023-08-19 22:09:49 -0700

    Added saved file info

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import streamlit as st
 import json
-from os import environ
+import os
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from PyPDF2 import PdfReader
 from docx import Document
 
 
 openai_api_key = ""
+
+db_path = os.path.join("data", "chroma_db")
+doc_index_path = os.path.join("data", "doc_index.json")
+
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-vectordb = Chroma(persist_directory="./data/chroma_db", embedding_function=embeddings)
+vectordb = Chroma(persist_directory=db_path, embedding_function=embeddings)
 
 
 def upload_file(file):
@@ -41,18 +46,81 @@ def upload_file(file):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.split_text(input_docs)
         
-        vectordb.add_texts(texts)
+        if (os.path.isfile(doc_index_path)):
+            with open(doc_index_path, "r") as openfile:
+                json_obj = json.load(openfile)
+
+            last_id = json_obj["last_id"]
+            ids = [str(i) for i in range(last_id, last_id + len(texts))]
+            metadata = []
+
+            for text in texts:
+                metadata.append({ "source": file.name })
+
+            vectordb.add_texts(texts, metadatas=metadata, ids=ids)
+            print("Embedding query done")
+
+            saved_docs = json_obj["saved_docs"]
+            saved_docs.append({
+                "source": file.name,
+                "ids": ids
+            })
+            dictionary = {
+                "last_id": last_id + len(texts),
+                "saved_docs": saved_docs
+            }
+
+            with open(doc_index_path, "w") as outfile:
+                json.dump(dictionary, outfile)
+        else:
+            last_id = 0
+            ids = [str(i) for i in range(last_id, last_id + len(texts))]
+            metadata = []
+
+            for text in texts:
+                metadata.append({ "source": file.name })
+
+            vectordb.add_texts(texts, metadatas=metadata, ids=ids)
+            print("Embedding query done")
+
+            dictionary = {
+                "last_id": last_id + len(texts),
+                "saved_docs": [{
+                    "source": file.name,
+                    "ids": ids
+                }]
+            }
+
+            with open(doc_index_path, "w") as outfile:
+                json.dump(dictionary, outfile)
 
 
 def generate_response(query):
     llm = OpenAI(temperature=0.2, openai_api_key=openai_api_key)
     llm_retriever = MultiQueryRetriever.from_llm(retriever=vectordb.as_retriever(), llm=llm)
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=llm_retriever)
+
+    if (query[-1] != "?"):
+        # err when no question mark?
+        query += "?"
+
     st.info(qa.run(query))
+    print("LLM query done")
+
+
+def list_saved_files():
+    if (os.path.isfile(doc_index_path)):
+        with open(doc_index_path, "r") as openfile:
+            json_obj = json.load(openfile)
+
+        saved_docs = json_obj["saved_docs"]
+
+        for doc in saved_docs:
+            st.text(doc["source"])
 
 
 def main():
-    openai_api_key = environ.get('OPENAI_API_KEY')
+    openai_api_key = os.environ.get('OPENAI_API_KEY')
     openai_api_key = st.sidebar.text_input('OpenAI API Key', value=openai_api_key, type='password')
     
     st.title('Note Q&A')
@@ -74,6 +142,7 @@ def main():
 
     st.markdown('#')
     st.subheader('Saved Files')
+    list_saved_files()
 
 
 if __name__ == '__main__':
