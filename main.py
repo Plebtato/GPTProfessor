79aac7767379a71e2e906a80575674326268350f
jordan <jordanf828@gmail.com>
2023-08-19 18:00:37 -0700

    Refactor

--- main.py
+++ main.py
@@ -9,42 +9,48 @@ from PyPDF2 import PdfReader
 from docx import Document
 import json
 
-st.title('Note Q&A')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-file = st.file_uploader('Upload files', type=["pdf", "docx", "csv", "txt"], )
 
-if file is not None:
-    if file.type == "application/pdf":
-        reader = PdfReader(file)
-        input_docs = ""
-        for page in reader.pages:
-            input_docs += page.extract_text() + " "
-    
-    elif file.type == "text/plain":
-        input_docs = file.read().decode()
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+vectordb = Chroma(persist_directory="./data/chroma_db", embedding_function=embeddings)
 
-    elif file.type == "text/csv":
-        input_docs = file.read().decode()
 
-    elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        docx = Document(file)
-        input_docs = ""
-        for paragraph in docx.paragraphs:
-            input_docs += paragraph.text + " "
-    
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_text(input_docs)
-    embeddings = OpenAIEmbeddings()
-    vectordb = Chroma(persist_directory="./data/chroma_db", embedding_function=embeddings)
-    vectordb.add_texts(texts)
+def upload_file(file):
+    if file is not None:
+        if file.type == "application/pdf":
+            reader = PdfReader(file)
+            input_docs = ""
+            for page in reader.pages:
+                input_docs += page.extract_text() + " "
+        
+        elif file.type == "text/plain":
+            input_docs = file.read().decode()
+
+        elif file.type == "text/csv":
+            input_docs = file.read().decode()
+
+        elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            docx = Document(file)
+            input_docs = ""
+            for paragraph in docx.paragraphs:
+                input_docs += paragraph.text + " "
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_text(input_docs)
+        
+        vectordb.add_texts(texts)
+
 
 def generate_response(query):
     llm = OpenAI(temperature=0.2, openai_api_key=openai_api_key)
     llm_retriever = MultiQueryRetriever.from_llm(retriever=vectordb.as_retriever(), llm=llm)
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=llm_retriever)
     st.info(qa.run(query))
+
+
+st.title('Note Q&A')
 
 with st.form('my_form'):
     text = st.text_area('Ask:', 'What are the three key pieces of advice for learning how to code?')
@@ -53,3 +59,6 @@ with st.form('my_form'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
+
+file = st.file_uploader('Upload files', type=["pdf", "docx", "csv", "txt"], )
+upload_file(file)
